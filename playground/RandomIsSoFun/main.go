9e48906df174f48e0b25d442ae06cafdf84eb602
Liu <taipeng2@fa19-cs425-g73-01.cs.illinois.edu>
2019-12-16 20:48:21 -0600

    save

--- playground/RandomIsSoFun/main.go
+++ playground/RandomIsSoFun/main.go
@@ -1,4 +1,4 @@
-package a
+package main
 
 import (
 	"fmt"
@@ -9,29 +9,85 @@ import (
 func HaveSomeLucky(){
 	var luck int = rand.Intn(100)
 	if luck > 98 {
-		fmt.Println("WOW!!!You're super lucky!")
+		fmt.Println("SSSSSSSSSSSSS")
 	}else if luck > 89 {
-		fmt.Println("Lukcy day!")
+		fmt.Println("SSS")
 	}else if luck > 79 {
-		fmt.Println("Have a good day!")
+		fmt.Println("SS")
 	}else if luck > 59 {
-		fmt.Println("Well, normal day")
+		fmt.Println("S")
 	}else if luck > 30 {
-		fmt.Println("Anyway, it's not so bad")
+		fmt.Println("R")
 	}else if luck > 2 {
-		fmt.Println("Emmmm...Be careful my boy...")
+		fmt.Println("N")
 	}else {
-		fmt.Println("Don't go outside!!!")
+		fmt.Println("Nothing")
 	}
 
 }
+//EPmap = event-posibility map
+func Roll(EPmap map[string]float64, t int) []string{
+
+	//function defined type
+	type event struct {
+		Name string
+		Range float64
+	}
+
+	//event accumulated posibility map
+	var eventList []*event
+	var total_posibility float64
+
+	for name, posibility := range EPmap {
+		if posibility < 0 {
+			fmt.Println("Please give me positive number!")
+			return []string{}
+		}
+		total_posibility += posibility
+		e := event{name, total_posibility}
+		eventList = append(eventList, &e)
+	}
+
+	//Roll t times
+	results := make([]string, t)
+
+	for i := 0; i < t; i++ {
+		//Get a random number from 1 to 10,000
+		rand.Seed(time.Now().UnixNano())
+		var res float64 = total_posibility * float64(rand.Intn(10000))/10000
+
+		//Search event
+		for _, event := range eventList {
+			if res < event.Range {
+				results[i] = event.Name
+				break
+			}
+		}
+	}
+
+	return results
+}
+
+func ReduceList(l []string) map[string]int {
+	res := make(map[string]int)
+
+	for _, item := range l {
+		if _, ok := res[item]; ok{
+			res[item]++
+		}else {
+			res[item] = 1
+		}
+	}
+
+	return res
+}
 
 func main() {
+	mymap := map[string]float64{"SSR" : 2.5, "SR" : 25, "R" : 62.8, "SSR-Card": 0.5, "SR-Card" : 3, "R-Card" : 6.2}
 
-	var count [10]int
-	for i:=0; i < 100000; i++{
-		rand.Seed(time.Now().UnixNano())
-		count[rand.Intn(10)]++
-	}
-	fmt.Println(count)
+	res := Roll(mymap, 100)
+	//fmt.Println(res)
+
+	resmap := ReduceList(res)
+	fmt.Println(resmap)
 }
